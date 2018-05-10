8fbc111d64a9e5e581d0c25fca89a371ee8c7442
ghkahxhd <ghkahxhd@daum.net>
2018-05-11 01:54:02 +0900

    modify filter.js   add func making canvas object

--- filter.js
+++ filter.js
@@ -1,1 +1,17 @@
 console.log('filter init');
+
+// make canvas
+var canvas = $('#canvas')[0];
+var ctx = canvas.getContext('2d');
+
+function drawImageData(image) {
+  image.height *= canvas.offsetWidth / image.width;
+  image.width = canvas.offsetWidth;
+
+  if (image.height > canvas.offsetHeight) {
+    image.width *= canvas.offsetHeight / image.height;
+    image.height = canvas.offsetHeight;
+  }
+
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+}
