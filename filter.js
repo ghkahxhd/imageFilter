cdf513242c66c47bd58d5d956f61fcbe86a47e08
ghkahxhd <ghkahxhd@daum.net>
2018-05-11 02:05:16 +0900

    add Invertfilter

--- filter.js
+++ filter.js
@@ -1,33 +1,55 @@
-console.log('filter init');
-
-// make canvas obj
+// canvas 객체 생성
 var canvas = $('#canvas')[0];
 var ctx = canvas.getContext('2d');
 
 function drawImageData(image) {
-  image.height *= canvas.offsetWidth / image.width;
-  image.width = canvas.offsetWidth;
+    image.height *= canvas.offsetWidth / image.width;
+    image.width = canvas.offsetWidth;
 
-  if (image.height > canvas.offsetHeight) {
-    image.width *= canvas.offsetHeight / image.height;
-    image.height = canvas.offsetHeight;
-  }
+    if(image.height > canvas.offsetHeight){
+        image.width *= canvas.offsetHeight / image.height;
+        image.height = canvas.offsetHeight;
+    }
 
-  ctx.drawImage(image, 0, 0, image.width, image.height);
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+    console.log(ctx.getImageData(0,0, canvas.width, canvas.height));
 }
 
-// add input button
+// click input button
 $('#loadButton').on('change', function (e) {
-  var file = e.target.files[0];
-  var fileReader = new FileReader();
+    var file = e.target.files[0];
+    var fileReader = new FileReader();
 
-  fileReader.onload = function(e) {
-    var image = new Image();
-    image.src = e.target.result;
-    image.onload = function() {
-      drawImageData(image);
-    }
-  };
+    fileReader.onload = function (e) {
+        var image = new Image();
+        image.src = e.target.result;
+        image.onload = function () {
+            drawImageData(image);
+        }
+    };
 
-  fileReader.readAsDataURL(file);
+    fileReader.readAsDataURL(file);
 });
+
+$('#filterButton').on('click', function () {
+    // imageData를 가져온다.
+    var pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
+
+    // image processing
+    var filteredData = invertFilter(pixels);
+
+    // Canvas에 다시 그린다.
+    ctx.putImageData(filteredData, 0 , 0);
+});
+
+// Filters
+function invertFilter(pixels) {
+    var d = pixels.data;
+    for(var i=0; i<pixels.data.length; i+=4 ){
+        d[i] = 255 - d[i];     // R
+        d[i+1] = 255 - d[i+1]; // G
+        d[i+2] = 255 - d[i+2]; // B
+        d[i+3] = 255;          // Alpha
+    }
+    return pixels;
+}
